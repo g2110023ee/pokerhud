59e95bb0642fa0699f03d1d8c3ccc5fa674f35bc
Ken Kuroiwa <coder@ken.aero>
2017-07-14 18:59:42 +0900

    Add new activity for game function. And making the menu on TopActivity. Also, provide change activity function to game from top.

--- app/src/main/kotlin/club/bluegem/pokerhud/TopActivity.kt
+++ app/src/main/kotlin/club/bluegem/pokerhud/TopActivity.kt
@@ -1,12 +1,30 @@
 package club.bluegem.pokerhud
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class TopActivity : AppCompatActivity() {
+class TopActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.top)
+        val game_button: Button = findViewById(R.id.button_game) as Button
+        game_button.setOnClickListener(this)
+        val result_button: Button = findViewById(R.id.button_result) as Button
+        game_button.setOnClickListener(this)
+    }
+
+    fun changeActivity(view: View) {
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onClick(v: View?){
+        if (v != null) {
+            changeActivity(v)
+        }
     }
 }
