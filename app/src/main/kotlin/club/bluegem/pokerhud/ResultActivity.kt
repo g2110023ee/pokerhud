3ccf81013fbd31e16401f071228aea988b7369f8
Ken Kuroiwa <coder@ken.aero>
2017-07-18 19:09:31 +0900

    Implemented ResultActivity's interfaces

--- app/src/main/kotlin/club/bluegem/pokerhud/ResultActivity.kt
+++ app/src/main/kotlin/club/bluegem/pokerhud/ResultActivity.kt
@@ -2,11 +2,17 @@ package club.bluegem.pokerhud
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.result)
+        val result = listOf("seat1", "seat2", "seat3", "seat4", "seat5", "seat6", "seat7", "seat8", "seat9", "seat10")
+        val resultList: ListView = findViewById(R.id.ResultList) as ListView
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, result)
+        resultList.adapter = adapter
     }
 }
