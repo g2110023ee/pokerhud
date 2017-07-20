37fa305240c41e92e954dd7e838f2ffb0aae6ad8
Ken Kuroiwa <coder@ken.aero>
2017-07-20 16:17:17 +0900

    Game Activity ・計算処理の実装が終了 ・レイアウトの修正 ・BindingによるList内各データの自動変更 ・それぞれにコメントを追加

--- app/src/main/kotlin/club/bluegem/pokerhud/TopActivity.kt
+++ app/src/main/kotlin/club/bluegem/pokerhud/TopActivity.kt
@@ -15,31 +15,38 @@ class TopActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("Status", "Open")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.top)
-        Log.d("Status", "Open")
+
+        //Buttonの機能配置
+        //Go to HUD(Game activity)
         val game_button: Button = findViewById(R.id.button_game) as Button
         game_button.setOnClickListener(object:View.OnClickListener {
             override fun onClick(v: View?) {
                 Log.d("Status", "onClick")
-                if (v != null) changeHudActivity(v)
+                if (v != null) changeHudActivity()
             }
         })
+        //Go to Result activity
         val result_button: Button = findViewById(R.id.button_result) as Button
         result_button.setOnClickListener(object:View.OnClickListener {
             override fun onClick(v: View?) {
-                if (v != null) changeResultActivity(v)
+                if (v != null) changeResultActivity()
             }
         })
+
+        //Facebook login button
         val facebook_button: Button = findViewById(R.id.button_facebook) as Button
         facebook_button.setOnClickListener(this)
     }
-        fun changeHudActivity(view: View) {
+
+        //GameActivityにインテントを飛ばすメソッド
+        fun changeHudActivity() {
             val intent = Intent(this, GameActivity::class.java)
             startActivity(intent)
         }
-        fun changeResultActivity(view: View) {
+        //ResultActivityにインテントを飛ばすメソッド
+        fun changeResultActivity() {
             val intent = Intent(this, ResultActivity::class.java)
             startActivity(intent)
         }
